cbf9a9377ec8a937a08ac10bb2dea5f3e048959d
Peter Minarik <peter@peterminarik.com>
2018-07-03 10:45:28 +0200

    Add intent & inten ui targets

---
+++ Intent/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  Intent
+//
+//  Created by Peter Minarik on 03.07.18.
+//  Copyright © 2018 Peter Minarik. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
